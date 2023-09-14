a74dd5226443fd748547cc131ec015bd221c1bf1
Hussein6685 <husseinail6685@gmail.com>
2023-09-14 21:58:03 +0300

    110  commit

--- main.js
+++ main.js
@@ -2984,85 +2984,233 @@ let myP = document.querySelector("p");
   - Math Object Methods
 */
 
-let n1 = [10, 30, 10, 20];
-let n2 = [30, 20, 10];
+// let n1 = [10, 30, 10, 20];
+// let n2 = [30, 20, 10];
 
-console.log("Your Solution Here"); // 210
+// console.log("Your Solution Here"); // 210
 
 
 
 
 
+/*
+  Regular Expression
+  - Email
+  - IP
+  - Phone
+  - URL
+*/
 
+// let str1 = '10 20 100 1000 5000';
+// let str2 = 'Os1 Os12 Os123 Os123Os Os12312Os123';
 
+// let invalidEmail = 'Osama@@@gmail....com';
+// let validEmail = 'o@nn.sa';
 
+// let ip = '192.168.2.1'; // IPv4
 
+// let url = 'elzero.org';
+// let url = 'elzero.org/';
+// let url = 'http://elzero.org/';
+// let url = 'http://www.elzero.org/';
+// let url = 'https://.elzero.org/';
+// let url = 'https://www.elzero.org/';
+// let url = 'https://www.elzero.org/?facebookid=asdasdasd';
 
 
 
 
 
 
+/*
+  Regular Expression
 
+  Syntax
+  /pattern/modifier(s);
+  new RegExp("pattern", "modifier(s)")
 
+  Modifiers => Flags
+  i => case-insensitive
+  g => global
+  m => Multilines
 
+  Search Methods
+  - match(Pattern)
 
+  Match
+  -- Matches A String Against a Regular Expression Pattern
+  -- Returns An Array With The Matches
+  -- Returns null If No Match Is Found.
+*/
 
 
+// let myString = "Hello Elzero Web School I Love elzero";
 
+// let regx = /elzero/ig;
 
+// console.log(myString.match(regx));
 
 
 
 
+/*
+  Regular Expression
 
+  Ranges
 
+  - Part 1
+  (X|Y) => X Or Y
+  [0-9] => 0 To 9
+  [^0-9] => Any Character Not 0 To 9
+  Practice
 
+  - Part 2
+  [a-z]
+  [^a-z]
+  [A-Z]
+  [^A-Z]
+  [abc]
+  [^abc]
 
+*/
 
+// let tld = "Com Net Org Info Code Io";
 
+// let tldRe = /(info|org|io)/ig;
+// console.log(tld.match(tldRe));
 
+// let nums = "12345678910";
+// let numsRe = /[0-2]/g;
+// console.log(nums.match(numsRe));
 
+// let notnNums = "12345678910";
+// let notNumsRe = /[^0-2]/g;
+// console.log(notnNums.match(notNumsRe));
 
+// let specialNums = "1!2@3#4$5%678910";
+// let specialNumsRe = /[^0-9]/g;
+// console.log(specialNums.match(specialNumsRe));
 
 
+// let Practice = "os1 os10s os2 os8 os8os";
+// let PracticeRe = /os[5-9]os/g;
+// console.log(Practice.match(PracticeRe));
 
 
 
 
+  // - Part 2
+  // [a-z]
+  // [^a-z]
+  // [A-Z]
+  // [^A-Z]
+  // [abc]
+  // [^abc]
 
+// let myString = "AaBbcdefG123!234%^&*";
+// let atozSmall = /[a-z]/g;
+// let notAtozSmall = /[^a-z]/g;
+// let atozCapital = /[A-Z]/g;
+// let notAtozCapital = /[^A-Z]/g;
+// let aAndcAnde = /[ace]/g;
+// let notaAndcAnde = /[^ace]/g;
+// let lettersCapsAndSmall = /[a-zA-Z]/g;
+// let numsAndSpecials = /[^a-zA-Z]/g;
+// let specials = /[^a-zA-Z0-9]/g;
+// // let speceials = /[^a-zA-z0-9]/g;
+// console.log(myString.match(atozSmall));
+// console.log(myString.match(notAtozSmall));
+// console.log(myString.match(atozCapital));
+// console.log(myString.match(notAtozCapital));
+// console.log(myString.match(aAndcAnde));
+// console.log(myString.match(notaAndcAnde));
+// console.log(myString.match(lettersCapsAndSmall));
+// console.log(myString.match(numsAndSpecials));
+// console.log(myString.match(specials));
 
 
 
 
 
 
+/*
+  Regular Expression
+  Character Classes
+  . => matches any character, except newline or other line terminators.
+  \w => matches word characters. [a-z, A-Z, 0-9 And Underscore]
+  \W => matches Non word characters
+  \d => matches digits from 0 to 9.
+  \D => matches non-digit characters.
+  \s => matches whitespace character.
+  \S => matches non whitespace character.
+*/
 
+// let email = 'O@@@g...com O@g.com O@g.net A@Y.com O-g.com o@s.org 1@1.com';
 
+// let dot = /./g;
+// let word = /\w/g;
+// let wWord = /\W/g;
+// let valid = /\w@\w.(com|net|org)/g;
+// console.log(email.match(dot));
+// console.log(email.match(word));
+// console.log(email.match(wWord));
+// console.log(email.match(valid));
 
 
 
 
 
+/*
+  Regular Expression
+  Character Classes
+  \b => matches at the beginning or end of a word.
+  \B => matches NOT at the beginning/end of a word.
 
+  Test Method
+  pattern.test(input)
+*/
 
+// let names = "Sayed 1Spam 2Spam 3Spam Spam4 Spam5 Osama Ahmed Aspamo";
 
 
+// let re = /(\bspam|spam\b)/ig;
+// console.log(names.match(re));
 
+// console.log(re.test(names));
+// console.log(/(\bspam|spam\b)/ig.test("osama"));
+// console.log(/(\bspam|spam\b)/ig.test("1Spam"));
+// console.log(/(\bspam|spam\b)/ig.test("Spam1"));
 
 
 
+/*
+  Regular Expression
 
+  Quantifiers
+  n+    => One Or More
+  n*    => zero or more
+  n?    => zero or one
+*/
 
 
 
+// let mails = "o@nn.sa osama@gmail.com elzero@gmail.net osama@mail.ru"; // All Emails
 
+// // let mailsRe = /\w+@\w+.(.com|net)/ig;
+// let mailsRe = /\w+@\w+.\w+/ig;
+// console.log(mails.match(mailsRe));
 
 
+// let nums = "0110 10 150 05120 0560 350 00"; // 0 Numbers Or No 0
 
+// let numsRe = /0\d*0/ig;
+// console.log(nums.match(numsRe))
 
 
+// let urls = "https://google.com http://www.website.net web.com"; // http + https
 
+// let urlsRe = /(https?:\/\/)?(www.)?\w+.\w+/ig;
+// console.log(urls.match(urlsRe));
 
 
 
@@ -3070,16 +3218,104 @@ console.log("Your Solution Here"); // 210
 
 
 
+/*
+  Regular Expression
 
+  Quantifiers
+  n{x}   => Number of
+  n{x,y} => Range
+  n{x,}  => At Least x
+*/
 
+// let serials = "S100S S3000S S50000S S950000S";
 
+// console.log(serials.match(/s\d{3}s/ig)); // S[Three Number]S
+// console.log(serials.match(/s\d{4,5}s/ig)); // S[Four Or Five Number]S
+// console.log(serials.match(/s\d{4,}s/ig)); // S[At Least Four]S
 
 
 
 
 
+/*
+  Regular Expression
 
+  Quantifiers
+  $  => End With Something
+  ^  => Start With Something
+  ?= => Followed By Something
+  ?! => Not Followed By Something
+*/
 
+// let myString = "We Love Programming";
+// let names = "1OsamaZ 2AhmedZ 3Mohammed 4MoustafaZ 5GamalZ";
+
+// console.log(/ing$/ig.test(myString));
+// console.log(/^we/ig.test(myString));
+// console.log(/lz$/ig.test(names));
+// console.log(/lz$/ig.test(names));
+// console.log(/^\d/ig.test(names));
+
+
+// console.log(names.match(/\d\w{5}(?=z)/ig));
+// console.log(names.match(/\d\w{8}(?!z)/ig));
+
+
+
+
+/*
+  Regular Expression
+
+  - replace
+  - replaceAll
+*/
+
+// let txt = "We Love Programming And @ Because @ Is Amazing";
+// console.log(txt.replace("@", "JavaScript"));
+// console.log(txt.replaceAll("@", "JavaScript"));
+// let re = /@/ig;
+// console.log(txt.replaceAll(re, "JavaScript"));
+// console.log(txt.replaceAll(/@/ig, "JavaScript"));
+
+
+
+//  onemli
+//مهم شرط عدد الالحرف والارقام في الفراغ
+/*
+  Regular Expression
+  - Input Form Validation Practice
+*/
+
+// document.getElementById("register").onsubmit = function () {
+//   let PhoneInput = document.getElementById("phone").value;
+//   let phoneRe = /\(\d{4}\)\s\d{3}-\d{4}/; // (1234) 567-8910
+//   let validationResult = phoneRe.test(PhoneInput);
+//   // console.log(validationResult);
+//   if (validationResult === false) {
+//     return false;
+//   }
+//   return true;
+// }
+
+
+/*
+  Regular Expression
+  - Challenge
+*/
+
+// let url1 = 'elzero.org';
+// let url2 = 'http://elzero.org';
+// let url3 = 'https://elzero.org';
+// let url4 = 'https://www.elzero.org';
+// let url5 = 'https://www.elzero.org:8080/articles.php?id=100&cat=topics';
+
+// let re = /.+/;
+
+// console.log(url1.match(re));
+// console.log(url2.match(re));
+// console.log(url3.match(re));
+// console.log(url4.match(re));
+// console.log(url5.match(re));
 
 
 
